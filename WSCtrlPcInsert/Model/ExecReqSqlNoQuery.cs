26c450a38a318177c93888277455d873f0820a66
jah4ck <johann.simon.web@gmail.com>
2016-11-29 15:59:36 +0100

    ajout connexion bdd

---
+++ WSCtrlPcInsert/Model/ExecReqSqlNoQuery.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace WSCtrlPcInsert.Model
+{
+    public class ExecReqSqlNoQuery
+    {
+        private string Requete;
+
+        public string req
+        {
+            get
+            {
+                return Requete;
+            }
+            set
+            {
+                Requete = value;
+                ExecuteReq(Requete);
+            }
+        }
+        public void ExecuteReq(string req)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=SRVPROD\ADMSRV;Initial Catalog=donnee_01;Persist Security Info=True;User ID=dev1;Password=Stine1!?";
+            SqlCommand cmd = new SqlCommand(req, conn);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+        }
+    }
+}
---
+++ WSCtrlPcInsert/Model/ExecReqSqlReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace WSCtrlPcInsert.Model
+{
+    public class ExecReqSqlReader
+    {
+        private string Myresult;
+
+        public string result
+        {
+            get
+            {
+                return Myresult;
+            }
+            set
+            {
+                Myresult = value;
+            }
+        }
+        public void ExecuteReq(string requete)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=SRVPROD\ADMSRV;Initial Catalog=donnee_01;Persist Security Info=True;User ID=dev1;Password=Stine1!?");
+            SqlCommand cmd = new SqlCommand(requete, conn);
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            int count = reader.FieldCount;
+            while (reader.Read())
+            {
+                string tmpligne = "";
+                for (int i = 0; i < count; i++)
+                {
+                    tmpligne = tmpligne + reader[i] + ";";
+
+                }
+                Myresult = Myresult + tmpligne + Environment.NewLine;
+            }
+            conn.Close();
+        }
+    }
+}
