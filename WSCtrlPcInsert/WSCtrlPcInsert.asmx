782da63c91fad9dc4e5f2af41619777469afade1
jah4ck <johann.simon.web@gmail.com>
2016-11-29 15:43:04 +0100

    first comit

---
+++ WSCtrlPcInsert/WSCtrlPcInsert.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WSCtrlPcInsert.asmx.cs" Class="WSCtrlPcInsert.WSCtrlPcInsert" %>
---
+++ WSCtrlPcInsert/WSCtrlPcInsert.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WSCtrlPcInsert
+{
+    /// <summary>
+    /// Description résumée de WSCtrlPcInsert
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSCtrlPcInsert : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+    }
+}
